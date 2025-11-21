f796aaa1b181938672eb2367bc05ee2ac73499eb
priyanka-xtown <priyanka@xtowns.in>
2025-11-21 17:12:41 +0530

    SupplierService.js for consistency and clarity

--- src/ims/pages/peoples/SuppliersService.js
+++ src/ims/pages/peoples/SuppliersService.js
@@ -1,28 +1,26 @@
 // src/services/SupplierService.js
-import api from "../../services/api.js";
+import api from "../../services/api";
 
 const SupplierService = {
   // GET all suppliers
   getSuppliers: (page = 1, limit = 10, search = "") =>
-    api.get(
-      `/suppliers/getAllSuppliers?page=${page}&limit=${limit}&search=${search}`
-    ),
+    api.get(`/suppliers/getAllSuppliers?page=${page}&limit=${limit}&search=${search}`),
+
+  // GET supplier by ID
+  getSupplierById: (id) =>
+    api.get(`/suppliers/getSupplierById/${id}`),
 
   // CREATE supplier
   createSupplier: (data) =>
     api.post("/suppliers/createsupplier", data),
 
-  // UPDATE supplier
+  // UPDATE supplier by ID
   updateSupplier: (id, data) =>
     api.put(`/suppliers/updateSupplierById/${id}`, data),
 
-  // DELETE supplier
+  // DELETE supplier by ID
   deleteSupplier: (id) =>
     api.delete(`/suppliers/deleteSupplierById/${id}`),
-
-  // GET supplier by ID
-  getSupplierById: (id) =>
-    api.get(`/suppliers/getSupplierById/${id}`),
 };
 
 export default SupplierService;
